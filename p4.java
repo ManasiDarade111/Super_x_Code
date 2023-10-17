0ee95fed0b5bd75df38d614e5dbe023298faa50e
Manasi Darade <104726010+ManasiDarade111@users.noreply.github.com>
2023-10-18 00:12:27 +0530

    Add files via upload

---
+++ p4.java
@@ -0,0 +1,22 @@
+
+//
+import java.util.*;
+public class p4 {
+
+    public static void main(String args[]){
+    
+         int s= 1;
+         int e=10;
+        
+
+         for(int i=s; i<e; i++)
+             if(i%2==1){
+            System.out.println( i);
+
+         }
+         
+
+
+    }
+    
+}
---
+++ p5.java
@@ -0,0 +1,14 @@
+
+// Count Size of String 
+public class p5 {
+public static void main(String[] args){
+
+    String str ="Manasi";
+
+    int size=str.length();
+    System.out.println(size);
+
+}
+
+    
+}
